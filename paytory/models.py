86e2110b758a901d0d516c212baf39cff668d829
Mani <mani.arasteh92@gmail.com>
2024-07-14 12:07:01 +0330

    Added incomes in model

--- paytory/models.py
+++ paytory/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,3 +9,16 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, models.CASCADE)
+
+    def __unicode__(self):
+        return f"{self.date}-{self.amount}"
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, models.CASCADE)
+
+    def __unicode__(self):
+        return f"{self.date}-{self.amount}"
