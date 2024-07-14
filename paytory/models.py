4504a952cc4b573bf9c23d9dcd3d8b49234f2012
Mani <mani.arasteh92@gmail.com>
2024-07-14 20:29:23 +0330

    Updated some files

--- paytory/models.py
+++ paytory/models.py
@@ -4,13 +4,20 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+
+    def __str__(self):
+        return f"{self.user}-token"
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, models.CASCADE)
 
-    def __unicode__(self):
+    def __str__(self):
         return f"{self.date}-{self.amount}"
 
 
@@ -20,5 +27,5 @@ class Income(models.Model):
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, models.CASCADE)
 
-    def __unicode__(self):
+    def __str__(self):
         return f"{self.date}-{self.amount}"
--- paytory/views.py
+++ paytory/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from .models import User, Token, Expense, Income
+from datetime import datetime
 
 # Create your views here.
+@csrf_exempt
+def submit_expense(request):
+    """User submits an expense"""
+
+    #TODO: validate data. User might be fake. Token might be fake. Amount might be...
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+    if not 'date' in request.POST:
+        date = datetime.now() #TODO: User might want to submit the date heself/herself
+
+    Expense.objects.create(user=this_user, amount=request.POST['amount'],
+            text=request.POST['text'], date=date)
+
+    return JsonResponse({
+        'status': 'ok',
+    }, encoder=JSONEncoder)
+
+@csrf_exempt
+def submit_income(request):
+    """User submits an expense"""
+
+    #TODO: validate data. User might be fake. Token might be fake. Amount might be...
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token=this_token).get()
+    if not 'date' in request.POST:
+        date = datetime.now() #TODO: User might want to submit the date himself/herself
+    Income.objects.create(user=this_user, amount=request.POST['amount'],
+            text=request.POST['text'], date=date)
+
+    return JsonResponse({
+        'status': 'ok',
+    }, encoder=JSONEncoder)
